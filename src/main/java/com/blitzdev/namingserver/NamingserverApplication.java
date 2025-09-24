75ef53b77a75f4baebb1492a370f34e9e558eb66
theKelvinCode <thekelvincode@gmail.com>
2025-09-24 21:58:31 +0100

    Updates to config-server,naming-server,gateway-server.

--- src/main/java/com/blitzdev/namingserver/NamingserverApplication.java
+++ src/main/java/com/blitzdev/namingserver/NamingserverApplication.java
@@ -2,8 +2,10 @@ package com.blitzdev.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NamingserverApplication {
 
 	public static void main(String[] args) {
